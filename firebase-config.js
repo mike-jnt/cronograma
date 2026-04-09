ef2287e4765ba59bf59a5c278066120e13c233fc
mike-jnt <josenatanaelramosrojo203@gmail.com>
2026-04-09 14:32:09 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+window.FIREBASE_OPTIONS = {
+  enabled: true,
+  workspaceId: 'equipo-1',
+  useAnonymousAuth: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAsa19N68Gu8TbmMD0sqavc0AG15WazzZ8',
+    authDomain: 'cronograma-3f158.firebaseapp.com',
+    projectId: 'cronograma-3f158',
+    storageBucket: 'cronograma-3f158.firebasestorage.app',
+    messagingSenderId: '981298917789',
+    appId: '1:981298917789:web:95267a4bf563210034ff74',
+    measurementId: 'G-XJFTR4DGQX'
+  }
+};
