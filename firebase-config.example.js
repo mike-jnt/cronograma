8c01e6b38984c4ae4cd2bbaf9618c3b4565a909c
mike-jnt <josenatanaelramosrojo203@gmail.com>
2026-04-09 13:28:56 -0500

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,13 @@
+window.FIREBASE_OPTIONS = {
+  enabled: false,
+  workspaceId: 'equipo-1',
+  useAnonymousAuth: true,
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
+  }
+};
